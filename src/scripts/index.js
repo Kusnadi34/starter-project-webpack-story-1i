f90c34035e6f6f1b97a742cd6bc927202285baec
Kusnadi34 <nanakusnadi035@gmail.com>
2026-04-28 21:48:54 +0700

    Fix service worker registration path

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,7 @@ import App from './pages/app.js';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register('sw.js').then(reg => {
       console.log('SW registered:', reg);
     }).catch(err => console.log('SW failed:', err));
   });
